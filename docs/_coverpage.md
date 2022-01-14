ccd08ec8d5e2dba030e38da14790524c0cc4bc4b
夏江龙 <xiajianglong@inspur.com>
2022-01-14 17:46:30 +0800

    dynamic suprise

--- docs/_coverpage.md
+++ docs/_coverpage.md
@@ -1,5 +1,8 @@
 ![logo](media/images/logo.svg)
-> 小时候最讨厌吃饭和睡觉，现在想想真贱。
+<blockquote>
+  <p id="suprise">小时候最讨厌吃饭和睡觉，现在想想真贱。</p>
+</blockquote>
+
 
 # FARRIS DEVKIT 文档
 
--- docs/public/share.js
+++ docs/public/share.js
@@ -1,46 +1,58 @@
 function share(hook, vm) {
-  function copyToClipboard(text) {
-    if (window.clipboardData && window.clipboardData.setData) {
-      return window.clipboardData.setData("Text", text);
-    }
-    else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
-      var textarea = document.createElement("textarea");
-      textarea.textContent = text;
-      textarea.style.position = "fixed";
-      document.body.appendChild(textarea);
-      textarea.select();
-      try {
-        return document.execCommand("copy");
-      }
-      catch (ex) {
-        console.warn("Copy to clipboard failed.", ex);
-        return prompt("Copy to clipboard: Ctrl+C, Enter", text);
-      }
-      finally {
-        document.body.removeChild(textarea);
-      }
-    }
-  }
-  hook.ready(function () {
-    var article = document.getElementById("main").parentElement;
-    if (!article) {
-      return;
-    }
-    var href = document.createElement("a");
-    href.href = "javascript:;";
-    href.text = "复制地址";
-    href.className = "share app-name-link";
-    href.onclick = function () {
-      var url = "https://igix.io/index.html" + window.location.hash;
-      copyToClipboard(url);
-      href.text = "已复制";
-      window.setTimeout(()=>{
-        href.text = "复制地址";
-      },1500);
-    };
-    article.appendChild(href);
-    console.log("ready");
-  });
+	function copyToClipboard(text) {
+		if (window.clipboardData && window.clipboardData.setData) {
+			return window.clipboardData.setData("Text", text);
+		} else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
+			var textarea = document.createElement("textarea");
+			textarea.textContent = text;
+			textarea.style.position = "fixed";
+			document.body.appendChild(textarea);
+			textarea.select();
+			try {
+				return document.execCommand("copy");
+			} catch (ex) {
+				console.warn("Copy to clipboard failed.", ex);
+				return prompt("Copy to clipboard: Ctrl+C, Enter", text);
+			} finally {
+				document.body.removeChild(textarea);
+			}
+		}
+	}
+	hook.ready(function() {
+		window.setTimeout(function(){
+			var supriseElement = document.getElementById("suprise");
+			if (supriseElement) {
+				var request = ajax({
+					method: 'get',
+					url: 'https://v1.hitokoto.cn/'
+				});
+
+				request.then(function(response) {
+					if (response && response.hitokoto) {
+						supriseElement.innerText = response.hitokoto;
+					}
+				});
+		}
+		},100);
+		var article = document.getElementById("main").parentElement;
+		if (!article) {
+			return;
+		}
+		var href = document.createElement("a");
+		href.href = "javascript:;";
+		href.text = "复制地址";
+		href.className = "share app-name-link";
+		href.onclick = function() {
+			var url = "https://igix.io/index.html" + window.location.hash;
+			copyToClipboard(url);
+			href.text = "已复制";
+			window.setTimeout(() => {
+				href.text = "复制地址";
+			}, 1500);
+		};
+		article.appendChild(href);
+		console.log("ready");
+	});
 }
 window.$docsify = window.$docsify || {};
 window.$docsify.plugins = [share].concat(window.$docsify.plugins || []);
